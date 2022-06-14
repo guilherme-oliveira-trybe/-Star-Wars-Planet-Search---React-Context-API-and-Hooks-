403a1f68c8adc2c20a8c2c8d2c5380b8c8c5cd80
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-06-14 17:33:31 -0300

    Finaliza css

--- src/component/Filters.jsx
+++ src/component/Filters.jsx
@@ -1,24 +1,29 @@
 import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
+import styles from './Filters.module.css';
 
 const Filters = () => {
   const { filterByNumericValues, deleteFilter } = useContext(StarWarsContext);
 
   return (
-    filterByNumericValues.map(({ column, operator, inputNumber }, index) => (
-      <div key={ `${column}-${index}` } data-testid="filter">
-        <span>
-          {`${column} ${operator} ${inputNumber}`}
-        </span>
-        <button
-          type="button"
-          onClick={ () => deleteFilter(index, column) }
-        >
-          X
+    <div className={ styles.container }>
+      {
+        filterByNumericValues.map(({ column, operator, inputNumber }, index) => (
+          <div key={ `${column}-${index}` } data-testid="filter">
+            <span>
+              {`${column} ${operator} ${inputNumber}`}
+            </span>
+            <button
+              type="button"
+              onClick={ () => deleteFilter(index, column) }
+            >
+              X
 
-        </button>
-      </div>
-    ))
+            </button>
+          </div>
+        ))
+      }
+    </div>
   );
 };
 
